9a3637c6bc56f31c8b3d3fa192c49a1e9ae34536
AlonsoFariasNavarro <alonso.farias@usm.cl>
2023-06-30 00:48:51 -0400

    a

--- app/Models/Cuenta.php
+++ app/Models/Cuenta.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cuenta extends Model
+class Cuenta extends Authenticatable
 {
     use HasFactory;
     protected $table = 'cuentas';
