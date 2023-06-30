19231b435e58112b6aa3541308e0437806b3fde7
AlonsoFariasNavarro <alonso.farias@usm.cl>
2023-06-30 00:40:02 -0400

    views

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,10 +1,10 @@
 @extends('templates.master')
 
 @section('contenido-principal')
-    <div class="container-fluid justify-content-center align-items-center">
+    <div class="container-fluid justify-content-center align-items-center bg-light">
         <div class="row">
             <!-- COL de los cards -->
-            <div class="col-9 d-flex justify-content-center ">
+            <div class="col d-flex justify-content-center ">
                 <div class="card m-3">
                     <div class="card-header align-middle text-center">
                         <h6>Titulo</h6>
@@ -32,34 +32,6 @@
                         <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum tortor non diam pharetra facilisis. Mauris scelerisque fermentum facilisis. Phasellus volutpat interdum sodales. Aliquam ex leo, iaculis a suscipit a, pellentesque dapibus ligula. Etiam in sapien dapibus, rutrum magna et, volutpat lacus. Aenean quam urna, bibendum id sodales eu, feugiat quis mauris. Proin tempor ante odio, et elementum arcu convallis et. Aliquam erat volutpat.</h4>
                     </div>
                 </div> 
-            </div>
-            <div class="col">
-            <div class="card m-3">
-                    <div class="card-header align-middle text-center">
-                        <h6>Titulo</h6>
-                    </div>
-                    <div class="card-body">
-                        <form action="#">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="col">
-                                        <label for="user">Ingrese Nombre de Usuario</label>
-                                        <input name="user" type="text">
-                                    </div>
-                                    <div class="col">
-                                        <br>
-                                        <label for="user">Ingrese Contraseña</label>
-                                        <input name="user" type="text">
-                                    </div>
-                                    <div class="col text-end">
-                                        <br>
-                                        <button class="brn btn-primary">Iniciar Sesion</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -13,18 +13,39 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-            @yield('if-nombre')
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="col-4"></div>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    @yield('if-contenido')
-                </ul>
-                <a href="{{ asset('/') }}" class="btn btn-primary d-flex justify-content-end pt-2 pb-1"><h6>Cerrar Sesion</h6></a>
-            </div>
+        <a class="navbar-brand" href="#">
+            <img src="{{asset('images/folder.png')}}" class="mx-4" style="height: 2rem;">
+            <p class="fw-bold">IMG Folder</p>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+            <a href="#" class="btn btn-success">Iniciar Sesion</a>
         </div>
+    </div>
     </nav>
 
     @yield('contenido-principal')
