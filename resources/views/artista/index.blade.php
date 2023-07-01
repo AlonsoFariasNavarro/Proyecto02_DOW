e41a2f51e9199ff3a6a4d5ac5840de2b113c75cc
Cristobal Macalapu <cristobal.macalapu@usm.cl>
2023-07-01 03:27:35 -0400

    Update index.blade.php
    123

--- resources/views/artista/index.blade.php
+++ resources/views/artista/index.blade.php
@@ -6,28 +6,25 @@
             <!-- COL de los cards -->
             <div class="col-6  justify-content-start mb-2">     
                 <div class="card ">
-                    <div class="card-header text-center">
+                    <div class="card-header text-center">                        
                         <h3>Mis fotos</h3>
-                        <div class="card-body">
-                            <div class="col d-flex "> 
-                            @foreach($imagenes as $num=>$imagen)
-                                @if(auth()->user()->user ==$imagen->cuenta_user)                              
-                                <div class="card m-3">
-                                    <div class="card-header align-middle text-center">
-                                        <h6>titulo</h6>
-                                    </div>                                   
-                                    <div class="card-body">
-                                        {{$imagen->titulo}}
-                                        <hr>
-                                    </div>                       
-                                </div>  
-                                @elseif($num==0 )
-                                <h3>No tienes fotos.</h3>
-                                @endif
-
-                            @endforeach  
-                            </div>                                                                                 
-                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="col d-flex "> 
+                        @foreach($imagenes as $num=>$imagen)
+                            @if(auth()->user()->user ==$imagen->cuenta_user)                              
+                            <div class="card m-3">
+                                <div class="card-header align-middle text-center">
+                                    <h6>titulo</h6>
+                                </div>                                   
+                                <div class="card-body">
+                                    {{$imagen->titulo}}
+                                    <hr>
+                                </div>                       
+                            </div>  
+                            @endif
+                        @endforeach  
+                        </div>                                                                                 
                     </div>
                 </div>               
             </div>
@@ -35,25 +32,25 @@
                 <div class="card ">
                     <div class="card-header ">
                         <h2 class="text-center" >Agregar foto</h2>
-                        <div class="card-body">
-                            <form method="POST" action="{{route('artista.agregar')}}">
-                                    @csrf   
-                                    <div class="mb-3">
-                                        <label for="" class="form-label">User</label>
-                                        <input type="text" class="form-control "disabled id="user"name="user" value="{{auth()->user()->user}}">
-                                    </div>                           
-                                    <div class="mb-3">
-                                        <label for="" class="form-label">Titulo de la imagen</label>
-                                        <input type="text" class="form-control" id="titulo"name="titulo">
-                                    </div>
-                                    <div>
-                                        <label for="formFileLg" class="form-label">SELECCIONA FOTO</label>
-                                        <input class="form-control form-control-lg" id="formFileLg" type="file">
-                                    </div>                                                          
-                                    <button type="submit" class="btn btn-primary m-3">Agregar usuario</button>                               
-                            </form>                            
-                        </div>
-                    </div>                
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{route('artista.agregar')}}">
+                                @csrf   
+                                <div class="mb-3">
+                                    <label for="" class="form-label">User</label>
+                                    <input type="text" class="form-control "disabled id="user"name="user" value="{{auth()->user()->user}}">
+                                </div>                           
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Titulo de la imagen</label>
+                                    <input type="text" class="form-control" id="titulo"name="titulo">
+                                </div>
+                                <div>
+                                    <label for="formFileLg" class="form-label">SELECCIONA FOTO</label>
+                                    <input class="form-control form-control-lg" id="formFileLg" type="file">
+                                </div>                                                          
+                                <button type="submit" class="btn btn-primary m-3">Agregar usuario</button>                               
+                        </form>                            
+                    </div>                                  
                 </div>
             </div>     
      
